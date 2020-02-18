9fc4a43bae1bd81397456b5f9d243c4487a2daef
Mateusz Boguszewski <mateo.b@data.pl>
2020-02-18 17:34:46 +0100

    UriService and README.MD

--- src/main/java/com/wikisearch/web/HomeController.java
+++ src/main/java/com/wikisearch/web/HomeController.java
@@ -1,10 +1,13 @@
 package com.wikisearch.web;
 
+import com.wikisearch.service.UriService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/")
@@ -12,7 +15,8 @@ public class HomeController {
 
     @ResponseBody
     @GetMapping("/query/club")
-    public String getClubNameFromUser(@RequestParam("name") String name) {
+    public String getClubNameFromUser(@RequestParam("name") String name) throws IOException {
+        new UriService().run(name);
         return name;
     }
 }
