edac029f813d9c6497c1ae5165b80037df081172
Mateusz Boguszewski <mateo.b@data.pl>
2020-02-18 17:06:58 +0100

    initial home controller

---
+++ src/main/java/com/wikisearch/web/HomeController.java
@@ -0,0 +1,18 @@
+package com.wikisearch.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @ResponseBody
+    @GetMapping("/query/club")
+    public String getClubNameFromUser(@RequestParam("name") String name) {
+        return name;
+    }
+}
