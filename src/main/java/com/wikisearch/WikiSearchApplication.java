c6688fc6036633f9f729384cfaa1ad9ae27908c1
Mateusz Boguszewski <mateo.b@data.pl>
2020-02-18 16:49:11 +0100

    initial commit

---
+++ src/main/java/com/wikisearch/WikiSearchApplication.java
@@ -0,0 +1,12 @@
+package com.wikisearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiSearchApplication.class);
+    }
+}
