89f5e272b5bd7a6249944d142eddf2467cd4a5a6
Mateusz Boguszewski <mateo.b@data.pl>
2020-02-18 19:20:22 +0100

    custom deserializer

---
+++ src/main/java/com/wikisearch/service/WikiPageDeserializer.java
@@ -0,0 +1,38 @@
+package com.wikisearch.service;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.wikisearch.WikiPage;
+
+import java.io.IOException;
+
+public class WikiPageDeserializer extends StdDeserializer<WikiPage> {
+
+    public WikiPageDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public WikiPage deserialize(JsonParser parser, DeserializationContext deserializer) throws IOException {
+        WikiPage wikiPage = new WikiPage();
+        while (!parser.isClosed()) {
+            JsonToken jsonToken = parser.nextToken();
+
+            if (JsonToken.FIELD_NAME.equals(jsonToken)) {
+                String fieldName = parser.getCurrentName();
+                System.out.println(fieldName);
+
+                jsonToken = parser.nextToken();
+
+                if ("title".equals(fieldName)) {
+                    wikiPage.setTitle(parser.getValueAsString());
+                } else if ("snippet".equals(fieldName)) {
+                    wikiPage.setSnippet(parser.getValueAsString());
+                }
+            }
+        }
+        return wikiPage;
+    }
+}
