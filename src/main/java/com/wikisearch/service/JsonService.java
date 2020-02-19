16b3e20636ae154d8815db537a38b4dfc0fc86bc
Mateusz Boguszewski <mateo.b@data.pl>
2020-02-19 19:45:01 +0100

    better service, simple parsing of json

--- src/main/java/com/wikisearch/service/JsonService.java
+++ src/main/java/com/wikisearch/service/JsonService.java
@@ -1,43 +1,32 @@
 package com.wikisearch.service;
 
-import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.wikisearch.WikiPage;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.net.URI;
-import java.util.LinkedList;
+import java.net.URL;
 import java.util.List;
 
-@Slf4j
 @Service
 public class JsonService {
 
     private ObjectMapper objectMapper;
-    private RestTemplate restTemplate;
-    private Iterable<WikiPage> wikiPages;
 
     public JsonService() {
         this.objectMapper = new ObjectMapper();
-        this.restTemplate = new RestTemplate();
     }
 
-    public void run(URI uri) throws IOException {
-        parseJson(uri);
+    public URL run(URL url) throws IOException {
+        var jNodeUrl = parseJson(url);
+        return new UrlService().selectUrlByCriteria(jNodeUrl);
     }
 
-    public void parseJson(URI uri) throws IOException {
-        List<WikiPage> wikiPages = new LinkedList<>();
-        SimpleModule module = new SimpleModule("JsonCustomDeserializer", new Version(3, 1, 8, null, null, null));
-        module.addDeserializer(WikiPage.class, new JsonCustomDeserializer(WikiPage.class));
+    public List<JsonNode> parseJson(URL url) throws IOException {
+        JsonNode jNode = objectMapper.readTree(url);
+        List<JsonNode> jNodeUrl = jNode.findValues("canonicalurl");
 
-        objectMapper.registerModule(module);
-        objectMapper.readValue(uri.toURL(), WikiPage.class);
-
+        return jNodeUrl;
     }
 
 }
